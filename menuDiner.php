a739769f65070e1aa91f0b51b90d8944b612c73d
Yan Gabriel <yangabrielreis@gmail.com>
2024-03-08 19:38:19 -0300

    first commit on php

---
+++ menuDiner.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="menuDiner.php" method="post">
+    <label>Quantity</label><br>
+    <input type="text" name="quantity"><br>
+    <input type="submit" value="total"><br>
+
+
+
+  </form>
+</body>
+</html>
+
+<?php
+  $item = "pizza";
+  $price = 4.99;
+  $quantity = $_POST["quantity"];
+  $total = null;
+  $total = $quantity * $price;
+
+  if($_POST["quantity"]!=null&&$_POST["quantity"]>0){
+  echo "you have ordered {$quantity}x {$item}";
+  echo "<br>";
+  echo "your total is R\${$total}";}
+?>
+
