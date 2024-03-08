421c39bbea1f47229927129aac1f159cf3d7339a
Yan Gabriel <yangabrielreis@gmail.com>
2024-03-08 19:44:27 -0300

    nome alterado para menu

--- menuDiner.php
+++ menuDiner.php
@@ -3,16 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Menu</title>
 </head>
 <body>
   <form action="menuDiner.php" method="post">
     <label>Quantity</label><br>
     <input type="text" name="quantity"><br>
     <input type="submit" value="total"><br>
-
-
-
   </form>
 </body>
 </html>
