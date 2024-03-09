78a542d366cf0b35e3319b383f6e225f33d0cf75
Yan Gabriel <yangabrielreis@gmail.com>
2024-03-09 14:19:43 -0300

    programa retornando capitais dos estados

--- curso/exampleAssociativeArrays/exampleAssociativeArrays.php
+++ curso/exampleAssociativeArrays/exampleAssociativeArrays.php
@@ -3,10 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Capitals</title>
 </head>
 <body>
-  
+  <form action="exampleAssociativeArrays.php" method="post">
+    <label>Enter a State</label>
+    <input type="text" name="state">
+    <input type="submit">
+  </form>
 </body>
 </html>
 
@@ -15,4 +19,15 @@
                     "Santa Catarina" => "Florianopolis",
                     "Goias" => "Goiania");
 
+  $state = $_POST["state"];
+
+  foreach($capitals as $key => $values) {
+    if($key==$state){
+      echo "<br>" . $values;
+      break;
+    }
+  }
+  if($state!=$key){
+    echo "<br>Invalid State<br>";
+  }
 ?>
