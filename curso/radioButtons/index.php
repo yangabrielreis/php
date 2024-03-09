61c16b3f0cbeaa93e04d5a3311ef0dba94dab78d
Yan Gabriel <yangabrielreis@gmail.com>
2024-03-09 14:48:21 -0300

    radio inicial

---
+++ curso/radioButtons/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    <input type="radio" name="creditCard" value="Visa">
+    <input type="radio" name="creditCard" value="Mastercard">
+    <input type="radio" name="creditCard" value="OingoBoingo">
+    <input type="submit" name="send" value="send">
+
+  </form>
+</body>
+</html>
