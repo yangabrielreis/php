d8fe7828e160799da961ea786647ded3284d5603
Yan Gabriel <yangabrielreis@gmail.com>
2024-03-09 11:42:06 -0300

    contas raio e area v2

---
+++ contasRaio/contasRaio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="contasRaio.php" method="post">
+    <label>raio</label>
+    <input type="text" name="raio">
+    <input type="submit" value="total"><br>
+  </form>
+</body>
+</html>
+
+<?php
+  $raio = $_POST["raio"];
+  $result = null;
+  $circunpherence = $raio * pi() * 2;
+  echo "the circunpherence is {$circunpherence}<br>";
+  $area = pow($raio, 2) * pi();
+  echo "the area is {$area}<br>";
+?>
