d22e30135e3ec9a1f519bfc7837f2f83e6221245
Yan Gabriel <yangabrielreis@gmail.com>
2024-03-11 20:30:19 -0300

    codigo checkboxes funcionando

--- curso/checkBoxes/index.php
+++ curso/checkBoxes/index.php
@@ -7,7 +7,21 @@
 </head>
 <body>
   <form action="index.php" method="post">
-    
+    <input type="checkbox" name="food[]" value="pizza"> Pizza <br>
+    <input type="checkbox" name="food[]" value="taco"> Taco <br>
+    <input type="checkbox" name="food[]" value="burguer"> Burguer <br>
+    <input type="checkbox" name="food[]" value="burrito"> Burrito <br>
+    <input type="submit" name="add" value="Add"> <br>
   </form>
 </body>
 </html>
+
+<?php
+  
+  if(isset($_POST["add"])){
+    $foodArray = $_POST["food"];
+    foreach($foodArray as $foods){
+      echo "{$foods}<br>";
+    }
+  }
+?>
